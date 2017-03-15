4dfc1ac0cc0bfad007466a0072751c2bbbcafe4c
Gary Gilbert <gjgilbert@gmail.com>
2017-03-15 10:49:12 -0400

    added datasync entity

---
+++ src/main/java/unicon/matthews/oneroster/AcademicSession.java
@@ -0,0 +1,13 @@
+package unicon.matthews.oneroster;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class AcademicSession {
+  private String sourcedId;
+  private Status status;
+  private Map<String, String> metadata;
+  private String title;
+  private LocalDate startDate;
+  private LocalDate endDate;
+}
