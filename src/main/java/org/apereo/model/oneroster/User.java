5de147f51b55bd0f638623fd77990e9ce6e60a7d
xchopin <xchopin54@gmail.com>
2019-03-12 15:54:28 +0100

    feat(user): add new attributes for OneRoster 1.1

--- src/main/java/org/apereo/model/oneroster/User.java
+++ src/main/java/org/apereo/model/oneroster/User.java
@@ -1,7 +1,10 @@
 package org.apereo.model.oneroster;
 
+import java.time.Instant;
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -12,20 +15,25 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
  * @author xchopin <xavier.chopin@univ-lorraine.fr>
  */
 @JsonDeserialize(builder = User.Builder.class)
+
 public final class User {
-  
+
+  public final class UserId {
+    public String type;
+    public String identifier;
+  }
+
   private String sourcedId;
   private Status status;
+  private Instant dateLastModified;
   private Map<String, String> metadata;
-  private String username;
-  private String userId;
-  private String givenName;
-  private String familyName;
+  private UserId[] userIds;
+  private boolean enabledUser;
+  private String username, givenName, familyName, middleName;
   private Role role;
-  private String identifier;
-  private String email;
-  private String sms;
-  private String phone;
+  private String identifier, email, sms, phone;
+  private String[] grades;
+  private String password;
   
   private User() {}
 
@@ -37,16 +45,24 @@ public final class User {
     return status;
   }
 
+  public Instant getDateLastModified() {
+    return dateLastModified;
+  }
+
   public Map<String, String> getMetadata() {
     return metadata;
   }
 
-  public String getUsername() {
-    return username;
+  public UserId[] getUserIds() {
+    return userIds;
   }
 
-  public String getUserId() {
-    return userId;
+  public boolean isEnabledUser() {
+    return enabledUser;
+  }
+
+  public String getUsername() {
+    return username;
   }
 
   public String getGivenName() {
@@ -55,6 +71,10 @@ public final class User {
 
   public String getFamilyName() {
     return familyName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
   }
 
   public Role getRole() {
@@ -76,160 +96,160 @@ public final class User {
   public String getPhone() {
     return phone;
   }
-  
+
+  public String[] getGrades() {
+    return grades;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+
+    if (o == null || getClass() != o.getClass()) return false;
+
+    User user = (User) o;
+
+    return new EqualsBuilder()
+            .append(enabledUser, user.enabledUser)
+            .append(sourcedId, user.sourcedId)
+            .append(status, user.status)
+            .append(dateLastModified, user.dateLastModified)
+            .append(metadata, user.metadata)
+            .append(userIds, user.userIds)
+            .append(username, user.username)
+            .append(givenName, user.givenName)
+            .append(familyName, user.familyName)
+            .append(middleName, user.middleName)
+            .append(role, user.role)
+            .append(identifier, user.identifier)
+            .append(email, user.email)
+            .append(sms, user.sms)
+            .append(phone, user.phone)
+            .append(grades, user.grades)
+            .append(password, user.password)
+            .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+            .append(sourcedId)
+            .append(status)
+            .append(dateLastModified)
+            .append(metadata)
+            .append(userIds)
+            .append(enabledUser)
+            .append(username)
+            .append(givenName)
+            .append(familyName)
+            .append(middleName)
+            .append(role)
+            .append(identifier)
+            .append(email)
+            .append(sms)
+            .append(phone)
+            .append(grades)
+            .append(password)
+            .toHashCode();
+  }
+
   @Override
   public String toString() {
     return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
   }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + ((familyName == null) ? 0 : familyName.hashCode());
-    result = prime * result + ((givenName == null) ? 0 : givenName.hashCode());
-    result = prime * result + ((identifier == null) ? 0 : identifier.hashCode());
-    result = prime * result + ((metadata == null) ? 0 : metadata.hashCode());
-    result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-    result = prime * result + ((role == null) ? 0 : role.hashCode());
-    result = prime * result + ((sms == null) ? 0 : sms.hashCode());
-    result = prime * result + ((sourcedId == null) ? 0 : sourcedId.hashCode());
-    result = prime * result + ((status == null) ? 0 : status.hashCode());
-    result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-    result = prime * result + ((username == null) ? 0 : username.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    User other = (User) obj;
-    if (email == null) {
-      if (other.email != null)
-        return false;
-    } else if (!email.equals(other.email))
-      return false;
-    if (familyName == null) {
-      if (other.familyName != null)
-        return false;
-    } else if (!familyName.equals(other.familyName))
-      return false;
-    if (givenName == null) {
-      if (other.givenName != null)
-        return false;
-    } else if (!givenName.equals(other.givenName))
-      return false;
-    if (identifier == null) {
-      if (other.identifier != null)
-        return false;
-    } else if (!identifier.equals(other.identifier))
-      return false;
-    if (metadata == null) {
-      if (other.metadata != null)
-        return false;
-    } else if (!metadata.equals(other.metadata))
-      return false;
-    if (phone == null) {
-      if (other.phone != null)
-        return false;
-    } else if (!phone.equals(other.phone))
-      return false;
-    if (role != other.role)
-      return false;
-    if (sms == null) {
-      if (other.sms != null)
-        return false;
-    } else if (!sms.equals(other.sms))
-      return false;
-    if (sourcedId == null) {
-      if (other.sourcedId != null)
-        return false;
-    } else if (!sourcedId.equals(other.sourcedId))
-      return false;
-    if (status != other.status)
-      return false;
-    if (userId == null) {
-      if (other.userId != null)
-        return false;
-    } else if (!userId.equals(other.userId))
-      return false;
-    if (username == null) {
-      if (other.username != null)
-        return false;
-    } else if (!username.equals(other.username))
-      return false;
-    return true;
-  }
-  
   public static class Builder {
-    private User _user = new User();
-
-    public Builder withSourcedId(String sourcedId) {
+    private User _user = new User()
+            ;
+    public User withSourcedId(String sourcedId) {
       _user.sourcedId = sourcedId;
-      return this;
+      return _user;
     }
-    
-    public Builder withStatus(Status status) {
+
+    public User withStatus(Status status) {
       _user.status = status;
-      return this;
+      return _user;
     }
-    
-    public Builder withMetadata(Map<String, String> metadata) {
+
+    public User withDateLastModified(Instant dateLastModified) {
+      _user.dateLastModified = dateLastModified;
+      return _user;
+    }
+
+    public User withMetadata(Map<String, String> metadata) {
       _user.metadata = metadata;
-      return this;
+      return _user;
     }
-    
-    public Builder withUsername(String username) {
+
+    public User withUserIds(UserId[] userIds) {
+      _user.userIds = userIds;
+      return _user;
+    }
+
+    public User withEnabledUser(boolean enabledUser) {
+      _user.enabledUser = enabledUser;
+      return _user;
+    }
+
+    public User withUsername(String username) {
       _user.username = username;
-      return this;
+      return _user;
     }
-    
-    public Builder withUserId(String userId) {
-      _user.userId = userId;
-      return this;
-    }
-    
-    public Builder withGivenName(String givenName) {
+
+    public User withGivenName(String givenName) {
       _user.givenName = givenName;
-      return this;
+      return _user;
     }
-    
-    public Builder withFamilyName(String familyName) {
+
+    public User withFamilyName(String familyName) {
       _user.familyName = familyName;
-      return this;
+      return _user;
     }
-    
-    public Builder withRole(Role role) {
+
+    public User withMiddleName(String middleName) {
+      _user.middleName = middleName;
+      return _user;
+    }
+
+    public User withRole(Role role) {
       _user.role = role;
-      return this;
+      return _user;
     }
-    
-    public Builder withIdentifier(String identifier) {
+
+    public User withIdentifier(String identifier) {
       _user.identifier = identifier;
-      return this;
+      return _user;
     }
-    
-    public Builder withEmail(String email) {
+
+    public User withEmail(String email) {
       _user.email = email;
-      return this;
+      return _user;
     }
-    
-    public Builder withSms(String sms) {
+
+    public User withSms(String sms) {
       _user.sms = sms;
-      return this;
+      return _user;
     }
-    
-    public Builder withPhone(String phone) {
+
+    public User withPhone(String phone) {
       _user.phone = phone;
-      return this;
+      return _user;
     }
-    
+
+    public User withGrades(String[] grades) {
+      _user.grades = grades;
+      return _user;
+    }
+
+    public User withPassword(String password) {
+      _user.password = password;
+      return _user;
+    }
+
     public User build() {
       return _user;
     }
